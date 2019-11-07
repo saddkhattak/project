c1404582353faefa225e6a1baae549b2aff222f8
saddkhattak <54858927+saddkhattak@users.noreply.github.com>
2019-11-07 10:14:45 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,165 @@
+<?php
+      require_once("include/header.php");
+?>
+<style>
+    .image {
+        margin-top: 100px;
+    
+    }
+    .text{
+        margin-top: 120px;
+        
+    }
+    *{
+    padding: none;
+    margin: none;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    
+}
+
+
+
+</style>
+          <!--Slider Stat Here-->
+<main role="main">
+
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="images/slide1.jpeg" class="bd-placeholder-img" width="100%" height="100%">
+        <!--<svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"/></svg>-->
+        <div class="container">
+          <div class="carousel-caption text-left">
+<!--            <h1>Breaking News</h1>-->
+<!--
+            <p>Islamia College Student got first position in the ETEA medical Entrance Test 2019 (2019-09-04 ) 
+</p>
+-->
+<!--            <p><a class="btn btn-lg btn-primary" href="#" role="button">View More...</a></p>-->
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+       <img src="images/slide2.jpeg" class="bd-placeholder-img" width="100%" height="100%">
+        <div class="container">
+          <div class="carousel-caption">
+<!--
+            <h1>Another example headline.</h1>
+            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+            <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+          </div>
+-->
+        </div>
+      </div>
+      <div class="carousel-item">
+       <img src="images/slide3.jpeg" class="bd-placeholder-img" width="100%" height="100%">
+        <div class="container">
+          <div class="carousel-caption text-right">
+
+            <h1>One more for good measure.</h1>
+            <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+            <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+            </div>
+            
+</div>
+        </div>
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+
+      <!-- SLider End Here
+
+
+        <!--Messages Section Start Here-->
+
+  <div class="container marketing">
+
+    <!-- Three columns of text below the carousel messge of the princple -->
+    <div class="row">
+      <div class="col-lg-4">
+        <img src="images/principle.jpeg" class="bd-placeholder-img rounded-circle" width="140" height="140">
+        <h2>Principle Messege</h2>
+        <p>Dear visitor, I appreciate your interest in the Islamia College Peshawar and feel privileged to welcome you to its official website. Since its establishment in 1913, this iconic institution has always been the dream destination of all youngsters aspiring for excellence in academics, sports, debates and other areas of personality-development .</p>
+        <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+      </div>
+        <!-- /.col-lg-4  messege of vice principle-->
+        
+      <div class="col-lg-4">
+        <img src="images/vc.jpg" class="bd-placeholder-img rounded-circle" width="140" height="140">
+        <h2>Vice Principle Messege</h2>
+        <p>Dear visitor, I appreciate your interest in the Islamia College Peshawar and feel privileged to welcome you to its official website. Since its establishment in 1913, this iconic institution has always been the dream destination of all youngsters aspiring for excellence in academics, sports, debates and other areas of personality-development .</p>
+        <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+      </div>
+        <!-- /.col-lg-4  messege of the md of school-->
+        
+      <div class="col-lg-4">
+        <img src="images/md.jpeg" class="bd-placeholder-img rounded-circle" width="140" height="140">
+        <h2>MD Messege</h2>
+        <p>Dear visitor, I appreciate your interest in the Islamia College Peshawar and feel privileged to welcome you to its official website. Since its establishment in 1913, this iconic institution has always been the dream destination of all youngsters aspiring for excellence in academics, sports, debates and other areas of personality-development .</p>
+        <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+      </div><!-- /.col-lg-4 -->
+    </div><!-- /.row -->
+
+      <!--Messages Section End HEre-->
+
+
+    <!-- FEATURETTES Area Start HEre -->
+
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+      <div class="col-md-6">
+        <h2 class="featurette-heading">History </h2>
+        <p class="lead">Launched on February 2, 1996  , Paradise Public School Karak was inaugurated on March 15, 1996. The School commenced with a mere 21 students who were led to school by eager and enthusiastic parents. The Managing Director of PPS Mr. Hidayat khan Khattak laid the foundation stone in the presence of elders of area and Chairman Gul Faraz khan Khattak. The PPS is headed by Mr. Muhammad Luqman Khattak under whose stewardship the school has grown up from strength to strength. 
+        Affiliated to BISE Kohat, Paradise Public School Karak provides holistic education and endeavors to prepare the citizens of the future. We believe that objectives of education include the formation habits of truthfulness, industry, spirit of enquiry, resilience, tolerance, self-confidence and humility. True education helps build a society which is equibtable and fair. We  endeavor to make students cognizant of their role as instruments of social transformation</p>
+      </div>
+      <div class="col-md-6 image">
+       <img src="images/slide1.jpeg" class="bd-placeholder-img bd-placeholder-img-lg featurette-image " width="600" height="500">
+      </div>
+    </div>
+
+      
+      
+      
+
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+      <div class="col-md-6 order-md-2">
+        <h2 class="featurette-heading"> Our Mission</h2>
+        <p class="lead"> '𝙎𝙚𝙧𝙫𝙞𝙘𝙚 𝘽𝙚𝙛𝙤𝙧𝙚 𝙎𝙚𝙡𝙛' is the ethos deeply ingrained in every despite. It signifies that the happiness,safety and comfort of others mean more to us than our own happiness,safety and comfort. The motto reminds us constantly that giving is nobler than receiving and that we give without expecting anything in return.
+Paradise
+Educating the nation since 1996</p>
+      </div>
+      <div class="col-md-6 order-md-1 image">
+        <img src="images/slide2.jpeg" class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid " width="600" height="500">
+      </div>
+    </div>
+
+
+   
+
+    <hr class="featurette-divider">
+
+      
+      
+      
+    <!-- /END THE FEATURETTES Area -->
+
+  </div> <!-- /.container -->
+
+
+    <?php  require_once("include/footer.php");?>
